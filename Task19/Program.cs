6e2b08888a523b9b484c292b1b10bb31ef9e5879
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-11-30 20:51:36 +0300

    Home tasks 19

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+var str = number.ToString();
+
+int start = 0;
+int finish = str.Length - 1;
+
+do
+{
+  if (str[start] == str[finish]) {
+    start++;
+    finish--;
+  } else {
+    Console.WriteLine("Это не палиндром!");
+    return;
+  }
+}
+while (start < finish);
+
+Console.WriteLine("Это палиндром!");
