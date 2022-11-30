e462d13239143d507a1d708db69616dafc600387
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-11-30 21:28:00 +0300

    Home tasks 23

---
+++ Task23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++) {
+    Console.WriteLine("Куб числа {0} равен {1}", i, Math.Pow(i, 3));
+}
